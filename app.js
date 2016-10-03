8e4fb046d6aba3c3f579e0eee2b140ef0945667e
Ford Fowler <fordjfowler@gmail.com>
2016-10-03 14:15:57 -0700

    build javascript shell/outline

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 'use strict';
 
 var productList = ['bag.jpg', 'bathroom.jpg',	'breakfast.jpg', 'chair.jpg',	'dog-duck.jpg', 'pen.jpg', 'scissors.jpg', 'sweep.png',	'unicorn.jpg', 'water-can.jpg', 'banana.jpg', 'boots.jpg', 'bubblegum.jpg', 'cthulhu.jpg', 'dragon.jpg', 'pet-sweep.jpg', 'shark.jpg', 'tauntaun.jpg', 'usb.gif', 'wine-glass.jpg'];
-
+var previousThree = [];
+var numProducts = productList.length;
+var imagesEl = document.getElementById('images');
 
 function product(filename) {
   this.name = filename.substring(0, filename.length - 4);
   this.filePath = 'img/' + filename;
   this.clicks = 0;
   this.timesShown = 0;
-  function this.percentageClicked() {
+  this.percentageClicked = function() {
     return 100 * clicks / timesShown;
   }
+};
+
+function selectThree() {
+
 }
+
+function renderThree() {
+
+}
+
+function handleClick(event) {
+  var target = event.target;
+  
+}
+
+imagesEl.addEventListener('click', handleClick);

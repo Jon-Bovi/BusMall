77462616476d7afc2840e8fba201083505a7ec4d
Ford Fowler <fordjfowler@gmail.com>
2016-10-03 15:16:48 -0700

    render 3 product images

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 var productNameList = ['bag.jpg', 'bathroom.jpg',	'breakfast.jpg', 'chair.jpg',	'dog-duck.jpg', 'pen.jpg', 'scissors.jpg', 'sweep.png',	'unicorn.jpg', 'water-can.jpg', 'banana.jpg', 'boots.jpg', 'bubblegum.jpg', 'cthulhu.jpg', 'dragon.jpg', 'pet-sweep.jpg', 'shark.jpg', 'tauntaun.jpg', 'usb.gif', 'wine-glass.jpg'];
 var productObjectList = [];
 var previousThree = [];
-var numProducts = productList.length;
+var currentThree = [];
+var numProducts = productNameList.length;
 var imagesEl = document.getElementById('images');
 
 function Product(filename) {
   this.name = filename.substring(0, filename.length - 4);
-  this.filePath = 'img/' + filename;
+  this.filePath = './img/' + filename;
   this.clicks = 0;
   this.timesShown = 0;
   this.percentageClicked = function() {
@@ -17,22 +18,41 @@ function Product(filename) {
 };
 
 function selectThree() {
-  var numSelected = 0
+  currentThree = [];
+  var numSelected = 0;
   do {
+    console.log('current three: ');
+    console.log(currentThree);
     var randomObj = chooseRandomProduct()
-    if (!isAlreadySelected(randomObj)) {
-      previousThree.push(randomObj);
+    if (!isAlreadySelected(randomObj) && !wasShownPreviously(randomObj)) {
+      console.log('Adding ' + randomObj.name + ' to currentThree') ;
+      currentThree.push(randomObj);
       ++numSelected;
     }
   } while (numSelected < 3);
+  previousThree = [];
+  previousThree = currentThree.slice();
 }
 
 function isAlreadySelected(Obj) {
-  for (var i = 0; i < previousThree.length; i++) {
-    if (previousThree[i] === Obj) {
+  for (var i = 0; i < currentThree.length; i++) {
+    if (currentThree[i] === Obj) {
+      console.log(Obj.name + ' IS already selected.');
       return true;
     }
   }
+  console.log(Obj.name + ' is NOT already selected.');
+  return false;
+}
+
+function wasShownPreviously(Obj) {
+  for (var i = 0; i < previousThree.length; i++) {
+    if (Obj === previousThree[i]) {
+      console.log(Obj.name + ' WAS shown previously.');
+      return true;
+    }
+  }
+  console.log(Obj.name + ' was NOT shown previously.');
   return false;
 }
 
@@ -42,7 +62,11 @@ function chooseRandomProduct() {
 }
 
 function renderThree() {
-
+  for (var i = 0; i < 3; i++) {
+    var imgEl = document.createElement('img');
+    imgEl.setAttribute('src', currentThree[i].filePath);
+    imagesEl.appendChild(imgEl);
+  }
 }
 
 function handleClick(event) {
@@ -56,6 +80,17 @@ function makeObjectList() {
   }
 }
 
+function test() {
+  console.log('Previous three before: ');
+  console.log(previousThree);
+  selectThree();
+  renderThree();
+  console.log('Previous three after: ');
+  console.log(previousThree);
+}
+
 imagesEl.addEventListener('click', handleClick);
 
+
 makeObjectList();
+test();

d457a0e268f275773b67ca9474a1603baa6494f9
Ford Fowler <fordjfowler@gmail.com>
2016-10-03 16:02:00 -0700

    implement click event handler

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var previousThree = [];
 var currentThree = [];
 var numProducts = productNameList.length;
 var imagesEl = document.getElementById('images');
+var resultsEl = document.getElementById('results');
+var buttonEl = document.createElement('button');
+var setCount = 0;
 
 function Product(filename) {
   this.name = filename.substring(0, filename.length - 4);
@@ -21,14 +24,15 @@ function selectThree() {
   currentThree = [];
   var numSelected = 0;
   do {
-    console.log('current three: ');
-    console.log(currentThree);
     var randomObj = chooseRandomProduct()
     if (!isAlreadySelected(randomObj) && !wasShownPreviously(randomObj)) {
-      console.log('Adding ' + randomObj.name + ' to currentThree') ;
+      console.log('Adding ' + randomObj.name + ' to currentThree');
       currentThree.push(randomObj);
+      randomObj.timesShown += 1;
       ++numSelected;
     }
+    console.log('current three: ');
+    console.log(currentThree);
   } while (numSelected < 3);
   previousThree = [];
   previousThree = currentThree.slice();
@@ -71,8 +75,56 @@ function renderThree() {
 }
 
 function handleClick(event) {
-  var target = event.target;
 
+  var target = event.target;
+  console.log(target);
+  if (target.getAttribute('id') === 'images') {
+    return console.log('clicked...but not on image');
+  }
+  var targetObject = whichObject(target);
+  targetObject.clicks += 1;
+  ++setCount;
+  if (setCount >= 25) {
+    revealResultsButton();
+    return imagesEl.removeEventListener('click', handleClick);
+  } else {
+    selectThree();
+    renderThree();
+    console.log(setCount);
+  }
+}
+
+function revealResultsButton() {
+
+  buttonEl.setAttribute('type', 'button');
+  buttonEl.textContent = 'Show Results';
+  resultsEl.appendChild(buttonEl);
+
+  buttonEl.addEventListener('click', handleButtonClick);
+}
+
+function handleButtonClick() {
+  buttonEl.textContent = generateClickList();
+}
+
+function whichObject(targetEl) {
+  var i = 0;
+  while (i < 3) {
+    var name = targetEl.getAttribute('src');
+    if (name === currentThree[i].filePath) {
+      return currentThree[i];
+    }
+    ++i;
+  }
+  return false;
+}
+
+function generateClickList() {
+  var clickList = [];
+  for (var i = 0; i < numProducts; i++) {
+    clickList.push(productObjectList[i].name + ': ' + productObjectList[i].clicks + ' clicks');
+  }
+  return clickList;
 }
 
 function makeObjectList() {
@@ -94,4 +146,5 @@ imagesEl.addEventListener('click', handleClick);
 
 
 makeObjectList();
-test();
+selectThree();
+renderThree();
